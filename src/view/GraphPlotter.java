eceb9d64a58e1ce81a489759065ddf19d0e44f78
SinhaSoumyadeep <Mayukh@Soumyadeeps-MacBook-Pro.local>
2018-12-06 04:12:02 -0500

    graph plotter bug fix

--- src/view/GraphPlotter.java
+++ src/view/GraphPlotter.java
@@ -3,11 +3,12 @@ package view;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
-import org.jfree.ui.ApplicationFrame;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
 
-public class GraphPlotter extends ApplicationFrame {
+import javax.swing.*;
+
+public class GraphPlotter extends JFrame {
 
   public GraphPlotter( String applicationTitle) {
     super(applicationTitle);
@@ -26,6 +27,7 @@ public class GraphPlotter extends ApplicationFrame {
     ChartPanel chartPanel = new ChartPanel( lineChart );
     chartPanel.setPreferredSize( new java.awt.Dimension( 560 , 367 ) );
     setContentPane( chartPanel );
+    setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
   }
 
 
